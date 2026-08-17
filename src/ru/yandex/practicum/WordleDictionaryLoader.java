559d8ad77f28aff0ecfc83b92c35b1ebeec6fdd8
Lois <Loisnek@yandex.ru>
2026-08-17 16:21:16 +0300

    Исправить ошибки связанные с CheckStyle

--- src/ru/yandex/practicum/WordleDictionaryLoader.java
+++ src/ru/yandex/practicum/WordleDictionaryLoader.java
@@ -1,11 +1,9 @@
 package ru.yandex.practicum;
 
 import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
